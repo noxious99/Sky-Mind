e2fe574ee1673783ed3687324881379af33c6c66
noxious <rahman.rafiur.8132@gmail.com>
2026-05-31 04:27:36 +0600

    added tools for getting city cordinates and specific weather result

---
+++ src/tools/geo.tools.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+const getCoordinates = async (city) => {
+    const url =
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}`;
+
+    const { data } = await axios.get(url);
+
+    if (!data.results?.length) {
+        throw new Error("City not found");
+    }
+
+    const res = data.results[0];
+
+    return {
+        city: res.name,
+        lat: res.latitude,
+        lon: res.longitude
+    };
+}
+
+module.exports = {getCoordinates}
---
+++ src/tools/weather.tools.js
@@ -0,0 +1,12 @@
+const { getWeather } = require("../services/openMeteo");
+
+const getDailyWeather = async (lat, lon) => {
+    const data = await getWeather(lat, lon);
+
+    return {
+        temp: data.daily.temperature_2m_max[0],
+        rain: data.daily.precipitation_probability_max[0]
+    };
+}
+
+module.exports = { getDailyWeather };
